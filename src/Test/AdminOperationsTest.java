b7520606de6269a728ecffa78f88964c6f7d8c09
bugaj55113 <bbugi2016@gmail.com>
2024-04-08 11:30:50 +0200

    Test methods update

--- src/Test/AdminOperationsTest.java
+++ src/Test/AdminOperationsTest.java
@@ -1,6 +1,7 @@
 package Test;
 
 import App.Admin;
+import App.Customer;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -10,7 +11,7 @@ public class AdminOperationsTest {
         // Test dodawania klienta przez administratora
         Admin admin = new Admin();
 
-        admin.addCustomer("Jan Kowalski", "jankowalski@example.com");
+        admin.addCustomer("Jan Kowalski", "jankowalski@example.com", 123123123);
 
         assertTrue(admin.customerExist("jankowalski@example.com"));
     }
@@ -18,13 +19,27 @@ public class AdminOperationsTest {
     @Test
     public void admin_removeCustomer_shouldSucceed() {
         // Test usuwania klienta przez administratora
-        // Sprawdź, czy klient został usunięty poprawnie
+        Admin admin = new Admin();
+
+        admin.removeCustomer("jankowalski@example.com");
+
+        assertFalse(admin.customerExist("jankowalski@example.com"));
     }
 
     @Test
     public void admin_modifyCustomerSettings_shouldSucceed() {
         // Test modyfikowania ustawień klienta przez administratora
         // Sprawdź, czy ustawienia klienta zostały zmienione poprawnie
+        Admin admin = new Admin();
+
+        admin.addCustomer("Jan Kowalski", "jankowalski@example.com", 123123123);
+
+        Customer customer = admin.getCustometByMail("jankowalski@example.com");
+
+        admin.modifyCustomerSettings(customer, "Jan Nowak", 321321321);
+
+        assertEquals("Jan Nowak", customer.getName());
+        assertEquals(321321321, customer.getPhoneNumber());
     }
 
     @Test
